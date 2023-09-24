03219a5001bae368d60a18c560bc18e5f3ae6492
radhey-agnisys <122986161+radhey-agnisys@users.noreply.github.com>
2023-09-24 16:19:22 +0530

    created linked list added a few functionlity.

---
+++ linear/Node.java
@@ -0,0 +1,11 @@
+package linear;
+
+public class Node {
+
+    public Node  next;
+    public Node  prev;
+    public Object data;
+    public Node(Object data){
+        this.data = data;
+    }
+}
--- linear/linkedlist/MyLinkedList.java
+++ linear/linkedlist/MyLinkedList.java
@@ -1,125 +1,108 @@
 package linear.linkedlist;
 
-import javax.swing.*;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import linear.Node;
+import linear.list.MyList;
 
-public class MyLinkedList implements List {
+public class MyLinkedList<T> implements MyList{
+    private Node head, tail;
+    long size;
+    public MyLinkedList(){
+
+    }
 
     @Override
-    public int size() {
-        return 0;
+    public void add(Object data) {
+        if(head ==null){
+            head = new Node(data);
+            tail = head;
+        }else{
+            Node newNode = new Node(data);
+            //newNode
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+            //head.next = newNode;
+        }
+    }
+
+    @Override
+    public void remove(Object data) {
+
+    }
+
+    @Override
+    public void remove(long index) {
+        Node currNode = head;
+        long currIndex  = 0;
+        while(currNode.next != null){
+            if(currIndex == index){
+                currNode.prev.next = currNode.next;
+                currNode.next.prev = currNode.prev;
+                break;
+            }
+            currNode = currNode.next;
+            currIndex++;
+        }
+    }
+
+    @Override
+    public void clear() {head = null; tail = null;}
+
+    @Override
+    public Object get(long index) {
+        Node currNode = head;
+        long currIndex  = 0;
+        while(currNode.next != null && currIndex < index){
+            currNode = currNode.next;
+            currIndex++;
+            if (currIndex == index){
+                return currNode.data;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public long indexOf(Object data) {
+            Node currNode = head;
+            long currIndex  = 0;
+            while(currNode.next != null){
+                currNode = currNode.next;
+                currIndex++;
+                if(currNode.data == data){
+                    return currIndex;
+                }
+                currNode = currNode.next;
+                currIndex++;
+
+            }
+
+            return currNode.data == data ? currIndex : -1;
+
+
+    }
+
+    @Override
+    public boolean contains(Object data) {
+        Node currNode = head;
+        while(currNode.next != null) {
+            if (head.data == data)
+                return true;
+            currNode = currNode.next;
+        }
+        return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
-    public boolean contains(Object o) {
-        return false;
+    public long size() {
+        return size;
     }
+    public Node reverse(){
 
-    @Override
-    public Iterator iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public boolean add(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public Object get(int index) {
-        return null;
-    }
-
-    @Override
-    public Object set(int index, Object element) {
-        return null;
-    }
-
-    @Override
-    public void add(int index, Object element) {
-
-    }
-
-    @Override
-    public Object remove(int index) {
-        return null;
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public ListIterator listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List subList(int fromIndex, int toIndex) {
-        return null;
-    }
-
-    @Override
-    public boolean retainAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
     }
 }
